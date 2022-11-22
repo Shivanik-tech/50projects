445d6840111cef8f83b5f945d0c21e71841a59db
Shivani Prasad <prasadshivani234@gmail.com>
2022-11-22 08:51:39 +0530

    updated

--- expanding cards/script.js
+++ expanding cards/script.js
@@ -1,12 +1,26 @@
-const panels=document.querySelectorAll('.panels')
-panels.forEach((panel)=>{
-    panel.addEventListener('click',()=>{
+// const panels=document.querySelectorAll('.panel')
+// panels.forEach((panel)=>{
+//     panel.addEventListener('click',()=>{
+//         removeActiveClasses()
+//        panel.classList.add('active')
+//     })
+// })
+// function removeActiveClasses(){
+//     panels.forEach(panel=>{
+//         panels.classList.remove('active')
+//     })
+// }
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
         removeActiveClasses()
-       panel.classList.add('active')
+        panel.classList.add('active')
     })
 })
-function removeActiveClasses(){
-    panels.forEach(panel=>{
-        panels.classList.remove('active')
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
     })
 }
