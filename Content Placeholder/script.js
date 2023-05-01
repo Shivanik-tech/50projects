1cda16cbea0966725fa9e622cd908287de40839e
Shivani Prasad <prasadshivani234@gmail.com>
2023-05-01 08:07:23 +0530

    updated

--- Content Placeholder/script.js
+++ Content Placeholder/script.js
@@ -13,11 +13,11 @@ setTimeout(getData, 2500)
 function getData() {
   header.innerHTML =
     '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
-  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  title.innerHTML = 'Card 1'
   excerpt.innerHTML =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+    'Buy laptop at your own pace. Learn Digital Marketing and start your own business.'
   profile_img.innerHTML =
-    '<img src="https://randomuser.me/api/portraits/men/45.jpg" alt="" />'
+  '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
   name.innerHTML = 'Shivani Prasad'
   date.innerHTML = 'Mar 24, 2023'
 
